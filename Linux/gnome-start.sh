4f289646d504dbc1c3749bb7fcb490723d071563
netkawai <netkawai@gmail.com>
2023-01-05 22:06:13 -0500

    Add alternative way to start gnome-shell

--- Linux/gnome-start.sh
+++ Linux/gnome-start.sh
@@ -1,3 +1,7 @@
-XDG_SESSION_TYPE=wayland dbus-run-session gnome-session
+# force not to start x11 (only wayland)
+gnome-shell --wayland --no-x11
+# start gnome-session (with autostart xwayland in default)
+# XDG_SESSION_TYPE=wayland dbus-run-session gnome-session
+# stop automatic sleep
 # gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
 
