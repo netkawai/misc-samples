ccfaff56c1a518013d50f32cd34df6fe3ce56653
netkawai <netkawai@gmail.com>
2023-02-07 09:11:03 -0500

    Add another old sample

---
+++ enum_name.c
@@ -0,0 +1,28 @@
+﻿// const map
+#include <stdio.h>
+
+enum button_type {
+    CLOSE, 
+    MAXIMISE, 
+    MINIMISE
+};
+
+struct button_type_name
+{
+    enum button_type key;
+    char* name; 
+};
+
+static const struct button_type_name cursor_names[] = {
+    {CLOSE, "Close"},
+    {MAXIMISE, "Maximise"},
+    {MINIMISE, "Minimise"},
+};
+
+int main()
+{
+    for(int i = 0 ; i < sizeof(cursor_names)/sizeof(struct button_type_name); i++)
+    {
+        printf("%d, %s;", cursor_names[i].key, cursor_names[i].name);
+    }
+}
