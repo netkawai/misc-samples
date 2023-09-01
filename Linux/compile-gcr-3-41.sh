dbba68535074205fbc0f55fd882739b4f6ee8c5e
Masanori Kawai <netkawai@users.noreply.github.com>
2023-09-01 15:45:51 -0400

    Create compile-gcr-3-41.sh

---
+++ Linux/compile-gcr-3-41.sh
@@ -0,0 +1,2 @@
+# on Ubuntu 20.04 with disable gtk_doc
+sudo apt-get install libgcrypt-dev libglib2.0-dev cmake libp11-kit-dev libgtk-3-dev gobject-introspection valac libgirepository1.0-dev libsecret-1-dev libsystemd-dev
