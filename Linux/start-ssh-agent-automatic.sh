1d8571d343e6292ff421b3dc6031f5150e86816a
Masanori Kawai <netkawai@users.noreply.github.com>
2023-09-07 09:55:40 -0400

    Update start-ssh-agent-automatic.sh

--- Linux/start-ssh-agent-automatic.sh
+++ Linux/start-ssh-agent-automatic.sh
@@ -1,4 +1,10 @@
-if [ -z "$SSH_AUTH_SOCK" ] ; then
-    eval `ssh-agent`
+# only work on Ubuntu
+#
+systemctl --user start ssh-agent
+
+if [ $(ps ax | grep [s]sh-agent | wc -l) -gt 0 ] ; then
+    export SSH_AUTH_SOCK=${XDG_RUNTIME_DIR}/openssh_agent
     ssh-add
+else
+    echo "No ssh-agent !!!"
 fi
