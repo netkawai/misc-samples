a0db598c262ad38df46422ad83e641e3522f285f
Masanori Kawai <netkawai@users.noreply.github.com>
2024-01-26 10:46:57 -0500

    Update venv-python.cmd

--- Windows/venv-python.cmd
+++ Windows/venv-python.cmd
@@ -1,3 +1,3 @@
 REM You can use for this shortcut as well
 REM Some unknown reasons, command extension is not enabled even registry has a key to set command extension in default, so it is better to provide in cmd.exe option.
-cmd.exe /k /E:ON %USERPROFILE%\\.venv\\Scripts\\activate.bat
+cmd.exe /E:ON /k %USERPROFILE%\\.venv\\Scripts\\activate.bat
