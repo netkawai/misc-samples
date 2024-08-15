56337ef9799c70e762d94dc33752d44c90119610
Kawai <netkawai@users.noreply.github.com>
2024-08-15 13:13:12 -0400

    Update shell-string-condition.sh

--- Linux/shell-string-condition.sh
+++ Linux/shell-string-condition.sh
@@ -1,11 +1,21 @@
-# dash (busybox) this is NG
+# busybox(sh) 
 val = "NONE"
 checkVal = $(echo -n $val | grep -q "*UPDATE")
-# NG NG
+# NG NG 
 if [ -n $checkVal ]; then
-   echo "UPDATE icnlude false statement"
+   echo "UPDATE include false statement"
 fi
 # this is OK
 if [ -n "$checkVal" ]; then
    echo "UPDATE icnlude this will not appear"
 fi
+
+# other case
+# this is NG to show "ABC DEF"
+val="ABC";val="$val"$'\n'"DEF";echo $val
+# this is OK
+# ABC
+# DEF
+val="ABC";val="$val"$'\n'"DEF";echo "$val"
+
+# In shell double quuote is matter.
