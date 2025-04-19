1598ab6a27804d0e902dadb6f6542b0da112f667
netkawai <netkawai@gmail.com>
2025-04-19 07:47:10 -0400

    Remove env for debug specify window size

--- Linux/gnome-start-shell.sh
+++ Linux/gnome-start-shell.sh
@@ -1,7 +1,7 @@
 # force not to start x11 (only wayland)
 #gnome-shell --wayland --no-x11
 # start gnome-session (with autostart xwayland in default)
-MUTTER_DEBUG_DUMMY_MODE_SPECS=1024x768 dbus-run-session -- gnome-shell --nested --wayland
+dbus-run-session -- gnome-shell  --wayland --nested --no-x11
 # stop automatic sleep
 # gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
 
