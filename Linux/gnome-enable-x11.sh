af98bfaa0a56ad93b00ffdc6e3beeec863de34fa
netkawai <netkawai@gmail.com>
2025-12-07 16:50:23 -0500

    Update how to start gnome-session from tty login and deleted unapplicable

--- Linux/gnome-enable-x11.sh
+++
@@ -1,2 +0,0 @@
-sudo sed -i 's/--no-x11//' /usr/share/applications/org.gnome.Shell.desktop
-sudo mv /usr/lib/disabled-gsd-xsettings /usr/lib/gsd-xsettings
--- Linux/gnome-start.sh
+++ Linux/gnome-start.sh
@@ -1,7 +1,14 @@
 # force not to start x11 (only wayland)
 #gnome-shell --wayland --no-x11
 # start gnome-session (with autostart xwayland in default)
-XDG_SESSION_TYPE=wayland dbus-run-session gnome-session
+# dbus-run-session was deprecated 
+if [[ $(tty) == /dev/tty1 && $XDG_SESSION_TYPE == wayland ]]; then
+  gnome-session --no-reexec
+else
+  echo "Please create /etc/systemd/system/getty@tty1.service.d/wayland.conf"
+  echo "[Service]"
+  echo "Environment=XDG_SESSION_TYPE=wayland"
+fi
 # stop automatic sleep
 # gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
 
