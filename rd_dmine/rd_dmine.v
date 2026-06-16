b101150637feda89801a23e873c6622a83f0fc39
netkawai <netkawai@users.noreply.github.com>
2026-06-16 04:17:27 -0400

    Clean up public domain source codes  (keep only two)

---
+++ rd_dmine/rd_dmine.v
@@ -0,0 +1,10 @@
+// INTERNATIONAL AVS CENTRE - WARRANTY DISCLAIMER
+// Please read the file DISCLAIMER for conditions associated with this file.
+// avs@iavsc.org, www.iavsc.org
+
+flibrary ReadDatamine {
+   "../iac_proj/rd_dmine/r_dmmods.v" ReadDatamineMods;
+   "../iac_proj/rd_dmine/r_dmmacs.v" ReadDatamineMacs;
+   "../iac_proj/rd_dmine/r_dmapps.v" ReadDatamineApps;
+};
+
